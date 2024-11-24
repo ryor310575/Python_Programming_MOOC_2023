e5800a7e07c3792acfc8f6cc16e36781c49dbe1b
ryor310575 <ryor310575@gmail.com>
2024-11-24 17:27:39 -0500

    Adding files form Part 1 (last commit)

--- Daily wages.py
+++ Daily wages.py
@@ -13,3 +13,10 @@
 # Hours worked: 10
 # Day of the week: Sunday
 # Daily wages: 250.0 euros
+h_wage=float(input("Hourly wage: "))
+h_worked=float(input("Hours worked: "))
+d_week=input("Day of the week: ")
+d_wage=h_wage*h_worked
+if d_week=="Sunday":
+    d_wage=d_wage*2
+print(f"Daily wages: {d_wage} euros")
