7334164e152d86610f1f3745584ef10a2a9d51c9
ryor310575 <ryor310575@gmail.com>
2024-11-27 06:30:21 -0500

    Adding files form Part02 (Third commit)

--- part02/Alphabetically last.py
+++ part02/Alphabetically last.py
@@ -25,3 +25,12 @@
 # Please type in the 1st word: python
 # Please type in the 2nd word: python
 # You gave the same word twice.
+word_1=input("Please type in the 1st word: ")
+word_2=input("Please type in the 2nd word: ")
+if word_1.lower()>word_2.lower():
+    print(f"{word_1} comes alphabetically last.")
+elif word_2.lower()>word_1.lower():
+    print(f"{word_2} comes alphabetically last.")
+else:
+    print("You gave the same word twice.")
+
